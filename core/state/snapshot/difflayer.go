379e8adafc8ab404fa8a174d83eda2c18135b751
Chris Li <271678682li@gmail.com>
2024-12-04 17:49:10 +0800

    fix: fix base layer

--- core/state/snapshot/difflayer.go
+++ core/state/snapshot/difflayer.go
@@ -28,7 +28,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 	bloomfilter "github.com/holiman/bloomfilter/v2"
 )
